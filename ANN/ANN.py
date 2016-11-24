b3c6cb2815d8ed946e635cfa44d3bdfb19d09fbc
Shashank <sashankviswanadha@gmail.com>
2016-11-25 01:57:59 +0530

    ANN tested with and without noise

--- ANN/ANN.py
+++ ANN/ANN.py
@@ -26,14 +26,35 @@ class ANN():
         current_output = self.output_layer.compute(current_output)
         return current_output
 
-    def train(self,inputs,targets):
+    def train(self,inputs,targets,test_inputs,test_targets):
+        epochs = []
+        train_error = []
+        test_error = []
         for j in xrange(self.iterations):
             self.forward_pass(inputs)
             curr_data = self.output_layer.backprop(targets)
             for i in range(self.num_hidden_layers,0,-1):
                 curr_data = self.hidden_layers[i].backprop(curr_data)
-            if (j %50 == 0):
-                plt.plot(j,self.output_layer.mse,'ro')
+            test_outputs = self.forward_pass(test_inputs)
+            summ = summ2 = 0
+            for i in range(len(test_targets)):
+                for k in range(len(test_targets[0])):
+                    summ2 += (test_outputs[i][k] - test_targets[i][k])**2
+                    summ += abs(test_outputs[i][k] - test_targets[i][k])
+            avg_er = summ/(len(test_targets) * len(test_targets[0]))
+            mse = summ2/(2*len(test_targets) * len(test_targets[0]))
+            epochs.append(j)
+            train_error.append(self.output_layer.mse)
+            test_error.append(mse)
+
+        plt.plot(epochs,test_error,c = 'green',label = 'testing error')
+        plt.plot(epochs,train_error,c='red',label = 'training error')
+        plt.xlabel('epochs')
+        plt.ylabel('error')
+        plt.legend()
         plt.show()
+        print '----------------------------'
+        print 'tr er mse, avg : ',self.output_layer.mse,self.output_layer.avg_er
+        print 'te re mse , avg: ',summ,mse,avg_er
         temp = self.forward_pass(inputs)
         return temp
--- ANN/test_ANN.py
+++ ANN/test_ANN.py
@@ -4,18 +4,19 @@ from hidden_layer import *
 from input_layer import *
 from output_layer import *
 from ActivationFunction import *
-data = np.loadtxt("Data.txt")
+data = np.loadtxt("rmsout_28H_5000_RMSCost.txt")
 import matplotlib.pyplot as plt
+import random
 
-hid = {1:10}
+hid = {1:20}
 t0 = data[:,0]
-#t1 = data[:,1]
-#t2 = data[:,2]
+t1 = data[:,1]
+t2 = data[:,2]
 #t3 = data[:,3]
-te = data[:,1]
+te = data[:,4]
 #te[0] = te[0] - 3
 ma = mi = te[0]
-maa = mii = t0[0]
+#maa = mii = t0[0]
 for x in range(1,len(te)):
     #te[x] = te[x] - 3
     if te[x] > ma:
@@ -25,45 +26,58 @@ for x in range(1,len(te)):
 ra = ma - mi
 for x in range(len(te)):
     te[x] = (te[x]/ra) - (mi/ra)
+inp0_test = []
+inp1_test = []
+inp2_test = []
+inp0_train = []
+inp1_train = []
+inp2_train = []
+out_test = []
+out_train = []
+for x in range(len(te)):
+    if(x%5 == 0):
+        inp0_test.append(t0[x])
+        inp1_test.append(t1[x])
+        inp2_test.append(t2[x])
+        out_test.append(te[x])
+    else:
+        inp0_train.append(t0[x])
+        inp1_train.append(t1[x])
+        inp2_train.append(t2[x])
+        out_train.append(te[x])
 
-for x in range(1,len(t0)):
+'''for x in range(1,len(t0)):
     if t0[x] > maa:
         maa = t0[x]
     if t0[x] < mii:
         mii = t0[x]
 rat = maa - mii
 for x in range(0,len(t0)):
-    t0[x] = (t0[x] - mii)/rat
-inpp = np.array([t0]).T
-tarr = np.array([te]).T
+    t0[x] = (t0[x] - mii)/rat'''
+'''for o in range(len(out_train)):
+    out_train[o] = out_train[o] + random.uniform(0.0,0.05)'''
+
+inpp = np.array([inp0_train,inp1_train,inp2_train]).T
+tarr = np.array([out_train]).T
 inp = inpp
 tar = tarr
-np.savetxt('inp.txt',tar)
+inpp = np.array([inp0_test,inp1_test,inp2_test]).T
+tarr = np.array([out_test]).T
+inp_test = inpp
+out_te = tarr
+np.savetxt('input_training.txt',inp)
+np.savetxt('output_training.txt',tar)
+np.savetxt('output_testing.txt',out_te)
 af = sigmoid
-plt.plot(inp,tar,'ro')
-plt.show()
+#plt.plot(inp_test,out_te,'ro')
+#plt.show()
 #num_inputs,num_outputs,hidden_layers,non_linearity,learning_rate
-temp1 = np.array([[0,0,0],
-                  [0,0,1],
-                  [0,1,0],
-                  [0,1,1],
-                  [1,0,0],
-                  [1,0,1],
-                  [1,1,0],
-                  [1,1,1]])
-temp2 = np.array([[0,0,0,0,0,1,0],[0,1,1,0,0,1,1],[0,1,1,0,0,0,0],[0,1,0,1,0,0,1],[0,1,1,1,1,0,1],[0,1,0,0,1,0,0],[0,1,0,0,1,1,0],[1,1,1,1,1,1,1]])
-X = np.array([  [0,0,1],
-                [0,1,1],
-                [1,0,1],
-                [1,1,1],
-                [0,0,0]])
-
-# output dataset
-y = np.array([[0,1,1,0,1]]).T
-
-ann = ANN(1, 1, hid, af, 0.001, 10000)
-out = ann.train(inp,tar)
+#plt.plot(inp,tar,'ro',)
+#plt.show()
+ann = ANN(3, 1, hid, af, 0.001, 5000)
+t = ann.train(inp,tar,inp_test,out_te)
 #print out
+#print "ms error = ",ann.output_layer.mse,"avg error = ",ann.output_layer.avg_er
 """for x in range(len(out)):
     if out[x][0] > 0.5:
         out[x][0] = 1
@@ -71,11 +85,19 @@ out = ann.train(inp,tar)
         out[x][0] = 0"""
 '''for k in range(len(tar)):
     tar[k] = mii + out[k]*rat'''
-print out
-np.savetxt('test.txt', out)
-print "avg error = ",ann.output_layer.mse,ann.output_layer.avg_er
-plt.plot(inp,tar,'ro')
-plt.plot(inp,out,'ro')
+out = ann.forward_pass(inp_test)
+np.savetxt('obtained_output_training.txt', t)
+np.savetxt('obtained_output_testing.txt', out)
+
+add = 0
+tr = 0
+for x in range(len(out)):
+    add += abs(out[x][0] - out_te[x][0])
+for x in range(len(t)):
+    tr += abs(t[x][0] - tar[x][0])
+print 'train error : ',tr,tr/len(t)
+print 'test error : ',add,add/len(out)
+plt.plot(out_te,out,'ro',c='black')
 plt.show()
 """C = te - out
 np.savetxt('test.txt', C)
